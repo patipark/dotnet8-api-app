1170ef1368bded0f5b7853fa8ac70a9d3fa3c5e8
Patipark Sopha <iamwad@gmail.com>
2024-05-01 16:49:09 +0700

    get parameter from route path

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -7,5 +7,23 @@ namespace DotnetAPIApp.Controllers
     [ApiController]
     public class CompanyController : ControllerBase
     {
+
+        [Route("")] // localhost:port/api/company/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new { message = "Hello World /api/company/" }); // return Ok("Hello World");
+        }
+        [HttpGet("about")] // localhost:port/api/company/about
+        public IActionResult About()
+        {
+            return Ok(new { message = "company about us" });
+        }
+
+        [HttpGet("map/{id}")] // localhost:port/api/company/map/555
+        public IActionResult About([FromRoute] int id)
+        {
+            return Ok(new { message = $"Map Id: {id}" ,note = "[FromRoute] int id ==> calhost:port/api/company/map/555"});
+        }
     }
 }
