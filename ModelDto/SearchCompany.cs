202e53f052221b9eb6cf712af92d8a3a222b26ac
Patipark Sopha <iamwad@gmail.com>
2024-05-01 17:53:40 +0700

    DTO Model with Validate example

--- ModelDto/SearchCompany.cs
+++ ModelDto/SearchCompany.cs
@@ -5,9 +5,14 @@ using System.Text.Json.Serialization;
 namespace DotnetAPIApp.ModelsDto;
 
 public class SearchCompany
-{    
+{
+    [Required(ErrorMessage = "ชื่อบริษัทห้ามว่าง")]
     public string Name { get; set; } = null!;
+    [Range(100, 300, ConvertValueInInvariantCulture = false, ErrorMessage = "Code ต้องอยู่ระหว่าง 100 ถึง 300 เท่านั้น")]
     public int? Code { get; set; }
+
+    [StringLength(5, ErrorMessage = "location ห้ามเกิน 5 ตัวอักษร")]
+    [FromQuery(Name = "location")]
     public string? Province { get; set; }
-    
+
 }
